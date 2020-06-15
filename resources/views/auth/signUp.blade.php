686649339e868a0e528251c9b55613989c69dd46
TigranMnjoyan96 <mnjoyan1996@gmail.com>
2020-06-15 16:01:07 +0400

    --- Auto-Git Commit ---

--- resources/views/auth/signUp.blade.php
+++ resources/views/auth/signUp.blade.php
@@ -3,22 +3,15 @@
 
 @section('content')
     <h2>Sign Up</h2>
-    @if ($errors)
-        @foreach ($errors as $err)
-            <div class="alert-alert-danger">
-                {{ $err }}
-            </div>
-        @endforeach
-    @endif
     <form action="{{ route('signup') }}" method="POST" novalidate>
         @csrf
         <div class="form-group">
           <label for="email">Email address</label>
-          <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+          <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
         </div>
         <div class="form-group">
           <label for="password">Password</label>
-          <input type="password" class="form-control" name="pass" id="password" placeholder="Password">
+          <input type="password" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="pass" id="password" placeholder="Password">
         </div>
         <div class="form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
